407f2992f57be767ff93ba94c5f113db86558b6e
Johnethan Young <johnethany@icloud.com>
2025-07-11 15:08:14 -0500

    Update user.js
    Signed-off-by: Johnethan Young <johnethany@icloud.com>

--- models/user.js
+++ models/user.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true } // 👈 store hashed in production
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User', userSchema);
