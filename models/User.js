495cacdc7fda9d92d3f641cca43f8089e9e05091
Johnethan Young <johnethany@icloud.com>
2025-07-11 17:36:09 -0500

    Update User.js
    Signed-off-by: Johnethan Young <johnethany@icloud.com>

--- models/User.js
+++ models/User.js
@@ -1,11 +1,23 @@
 // models/User.js
-
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema({
-  username: String,
-  password: String,
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
   role: { type: String, default: 'user' }
+});
+
+// 🧂 Hash before saving
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = mongoose.model('User', UserSchema);
